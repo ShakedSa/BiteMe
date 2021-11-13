9d68cc57685a173313e6cfd158b1570f3b68b082
eden288 <edeneden288@gmail.com>
2021-11-13 17:20:42 +0200

    update2

--- BiteMeServer/src/Config/ReadPropertyFile.java
+++ BiteMeServer/src/Config/ReadPropertyFile.java
@@ -1,16 +1,33 @@
 package Config;
 
+import java.io.FileInputStream;
 import java.util.Properties;
 
 public class ReadPropertyFile {
+	
+	private static Properties prop;
+	private static ReadPropertyFile single_instance = null;
+	
+	public static ReadPropertyFile getInstance() 
+	{
+		if (single_instance == null) {
+            single_instance = new ReadPropertyFile();
+            
+			try {
+				prop=new Properties();
+				FileInputStream fin = new FileInputStream("C:/Users/Eden/git/BiteMe/BiteMeServer/src/Config/config.properties");
+				prop.load(fin);
+			}catch (Exception e) {}
+		}
 
-	public void main(String[] args) {
-		// TODO Auto-generated method stub
-		Properties prop=new Properties();
-		try {
-			prop.load(getClass().getResourceAsStream("/config.properties"));
-		}catch (Exception e) {}
-
+ 
+        return single_instance;
+		
 	}
-
+	public String getProp(String s) 
+	{
+		return prop.getProperty(s);
+		
+	}
 }
+
--- BiteMeServer/src/JDBC/mysqlConnection.java
+++ BiteMeServer/src/JDBC/mysqlConnection.java
@@ -1,5 +1,64 @@
 package JDBC;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 
 public class mysqlConnection {
 
+	public static String arg0= Config.ReadPropertyFile.getInstance().getProp("jdbcScheme");
+	public static String arg1= Config.ReadPropertyFile.getInstance().getProp("jdbcId");
+	public static String arg2= Config.ReadPropertyFile.getInstance().getProp("jdbcPass");
+	
+	public static void main(String[] args) 
+	{
+		try 
+		{
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+            System.out.println("Driver definition succeed");
+        } catch (Exception ex) {
+        	/* handle the error*/
+        	 System.out.println("Driver definition failed");
+        	 }
+        
+        try 
+        {
+            Connection conn = DriverManager.getConnection(arg0,arg1,arg2);
+           // Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/bm?serverTimezone=IST","root","1231233");
+        	System.out.println("SQL connection succeed");
+            printOrders(conn);
+     	} catch (SQLException ex) 
+     	    {/* handle any errors*/
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+            }
+
+   	}
+	
+	//this method stores the information of the orders table and display it to the user.
+	public static void printOrders(Connection con)
+	{
+		Statement stmt;
+		try 
+		{
+           // Connection con = DriverManager.getConnection(arg0,arg1,arg2);
+			stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM order;");
+	 		while(rs.next())
+	 		{
+				 // Print out the values
+				 System.out.println(rs.getString(1)+"  " +rs.getString(2)+"  " +rs.getString(3)+"  " +rs.getString(4)+"  " +rs.getString(5)+"  " +rs.getString(6));
+			} 
+			rs.close();
+		} catch (SQLException e) {e.printStackTrace();}
+	}
+	
+	
+	
+	
+	
 }
