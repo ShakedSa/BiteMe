2c3a637c6a592e328b4e877f70a57786c633a721
eden288 <edeneden288@gmail.com>
2021-12-25 11:00:29 +0200

    updated reports handler - bug fixes

--- BiteMeServer/src/ServerUtils/pdfConfigs.java
+++ BiteMeServer/src/ServerUtils/pdfConfigs.java
@@ -117,32 +117,10 @@ public class pdfConfigs {
 	}
 	
 	
-	
-	
-	public static JFreeChart generateBarChart() {
-		DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
-		dataSet.setValue(791, "Population", "2017");
-		dataSet.setValue(978, "Population", "2018");
-		dataSet.setValue(1262, "Population", "2019");
-		dataSet.setValue(1650, "Population", "2020");
-		dataSet.setValue(2519, "Population", "2021");
-		dataSet.setValue(6070, "Population", "today");
-
-		JFreeChart chart = ChartFactory.createBarChart(
-				"Aviel Gayness growth over the years", "Year", "Dicks sucked in millions",
-				dataSet, PlotOrientation.VERTICAL, false, true, false);
-
-		return chart;
-	}
-	
 	public static JFreeChart generateHist(ArrayList<Double> vals) {
+		if(vals.isEmpty())
+			return null;
 		double[] values = vals.stream().mapToDouble(Double::doubleValue).toArray();
-//        double[] values = { 95, 49, 14, 59, 50, 66, 47, 40, 1, 67,
-//                12, 58, 28, 63, 14, 9, 31, 17, 94, 71,
-//                49, 64, 73, 97, 15, 63, 10, 12, 31, 62,
-//                93, 49, 74, 90, 59, 14, 15, 88, 26, 57,
-//                77, 44, 58, 91, 10, 67, 57, 19, 88, 84                                
-//              };
         HistogramDataset dataset = new HistogramDataset();
         dataset.addSeries("key", values, 5);
 
