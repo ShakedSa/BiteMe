68a186e059d6008d172d9187e3c0cd57f31222f8
ShakedSa <sabagshaked99@gmail.com>
2021-12-26 22:42:45 +0200

    RestaurantMenuController - menu builder method some refactor.

--- BiteMeClient/src/Util/LoadingAnimation.java
+++ BiteMeClient/src/Util/LoadingAnimation.java
@@ -1,19 +1,39 @@
 package Util;
 
 import javafx.animation.RotateTransition;
+import javafx.animation.ScaleTransition;
+import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
 import javafx.util.Duration;
 
 public class LoadingAnimation {
 
-	private static RotateTransition rotateTransition;
-
+	/**
+	 * Processing animation.
+	 * 
+	 * @param Circle
+	 */
 	public static void LoadStart(Circle c) {
-		rotateTransition = new RotateTransition(Duration.seconds(10), c);
+		RotateTransition rotateTransition = new RotateTransition(Duration.seconds(10), c);
 		rotateTransition.setByAngle(360);
 		rotateTransition.setDelay(Duration.seconds(0));
 		rotateTransition.setRate(10);
 		rotateTransition.setCycleCount(18);
 		rotateTransition.play();
+	}
+
+	/**
+	 * Overlay popup animation.
+	 * 
+	 * @param Pane
+	 */
+	public static void overlayPaneTransition(Pane overlayPane) {
+		ScaleTransition st = new ScaleTransition(Duration.millis(300), overlayPane);
+		st.setFromX(0.5);
+		st.fromXProperty();
+		st.setFromY(0.5);
+		st.setToX(1);
+		st.setToY(1);
+		st.play();
 	}
 }
